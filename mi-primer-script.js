29697ff750b75ac8f241b565d83904e42522030f
jaan18 <jovas28@hotmail.com>
2021-01-26 19:38:18 -0600

    I just added some folders whit empty files

---
+++ mi-primer-script.js
@@ -0,0 +1,19 @@
+// One line comment
+/* Multi line comment */
+let a = 5;
+let b = 10;
+let c = 15;
+
+let operacion1 = (((3 + b) * c) / a) * 2;
+let operacion2 = ((a + b * c) / 5) * 2;
+let operacion3 = b / a + 2 * c;
+let operacion4 = (a + b + c / c) * a;
+let operacion5 = (3 * a) / c + a + b + c;
+let operacion6 = a - ((b + c) * a) / 1;
+
+console.log(operacion1);
+console.log(operacion2);
+console.log(operacion3);
+console.log(operacion4);
+console.log(operacion5);
+console.log(operacion6);

d50d68c16b7ace94115d39c14ea60c71b51b7c67
jaan18 <jovas28@hotmail.com>
2021-01-28 19:51:30 -0600

    Function Declaration

---
+++ 03-functions/app.js
@@ -0,0 +1,12 @@
+/* saludar("Jovani"); Function declaration 
+
+function saludar(nombre) {
+  console.log(`Hola ${nombre}`);
+}
+ */
+
+let saludar = (nombre) => `Hola ${nombre}`;
+console.log(saludar("Jovani"));
+
+let sumar = (a, b) => `Tu suma es: ${a + b}`;
+console.log(sumar(4, 5));
